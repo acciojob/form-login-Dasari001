d8dc41746f444b160a2d3129a6e68c24f3d15cbc
DASARI SAIBABU <107811202+Dasari001@users.noreply.github.com>
2023-05-02 12:15:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
- var fir = document.querySelector("input")[0].value;
- var las = document.querySelector("input")[1].value;
+ var fir = document.querySelector("input[name = fname]").value;
+ var las = document.querySelector("input[name = lname]")[1].value;
 		alert(fir+" "+las);
 }
